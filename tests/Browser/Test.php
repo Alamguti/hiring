23d15e25d6b6169f74ac74960456d5e8933a6f78
Alamguti <120285963+Alamguti@users.noreply.github.com>
2024-07-25 10:49:01 -0400

    Create Test.php

---
+++ tests/Browser/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class CompanyFormTest extends DuskTestCase
+{
+    public function test_view_contain_all_expected_elements(): void
+    {
+
+       
+    }
+}
